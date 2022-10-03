347f23d12da1340ab1a66c8c7f9a0b55dccb1df6
Tarek <83262890+tarek102@users.noreply.github.com>
2022-10-03 15:52:01 +0200

    Create Person, student and teacher classes

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Teacher < Person
+  attr_accessor @specialization
+
+  def initialize(name, age, parent_permission, specialization)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
